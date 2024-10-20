74935ea46f0115fc8e18a1989270a99c659cded7
Janardhan <janardhanpg12@gmail.com>
2024-10-20 15:58:05 +0530

    Add Task Styling

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
-import { Form, Input, Typography,Button } from "antd";
-import { useToast } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  useToast,
+  Heading,
+  Button,
+  Input,
+  Container,
+} from "@chakra-ui/react";
 
-const { Title } = Typography;
-
-interface FieldType {
-  taskName?: string;
-}
+const exTasks = [
+  "Do my laundry",
+  "Cancel milk delivery ",
+  "Clean fridge",
+  "Check passport",
+  "Do web check-in",
+  "Download a movie for the flight",
+  "Recharge mobile",
+  "Pack swimsuit",
+];
 
 interface AddTaskProps {
   onAddTask: (taskName: string) => void;
@@ -15,13 +28,14 @@ interface AddTaskProps {
 const AddTask: React.FC<AddTaskProps> = ({ onAddTask }) => {
   const toast = useToast();
   const [taskName, setTaskName] = useState("");
+  const randomIndex = Math.floor(Math.random() * exTasks.length);
 
   const handleSubmit = () => {
     if (taskName.trim() === "") {
       return;
     }
     onAddTask(taskName);
-    setTaskName("");
+
     toast({
       title: "Task added",
       description: taskName,
@@ -29,7 +43,7 @@ const AddTask: React.FC<AddTaskProps> = ({ onAddTask }) => {
       duration: 800,
       isClosable: true,
     });
-    // setTaskName("");
+    setTaskName("");
   };
 
   return (
@@ -41,36 +55,35 @@ const AddTask: React.FC<AddTaskProps> = ({ onAddTask }) => {
         margin: "20px",
       }}
     >
-      <Title level={2}>Add Tasks Here....</Title>
-
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: "700px", width: "100%" }} // Make form responsive
-        initialValues={{ remember: true }}
-        onFinish={handleSubmit}
-        autoComplete="off"
+      <Heading
+        as={"h2"}
+        size={"xl"}
+        textAlign={"center"}
+        marginTop={4}
+        marginBottom={2}
       >
-        <Form.Item<FieldType>
-          label="Task Name"
-          name="taskName"
-          rules={[
-            { required: true, message: "Please enter the name of the task!" },
-          ]}
-        >
+        Add Tasks Here....
+      </Heading>
+
+      <FormControl isRequired>
+        <Container maxW={"md"}>
+          <FormLabel>Task Name</FormLabel>
           <Input
             onChange={(e) => setTaskName(e.target.value)}
             value={taskName}
           />
-        </Form.Item>
+          <FormHelperText>e.g {exTasks[randomIndex]}</FormHelperText>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary"  htmlType="submit">
+          <Button
+            colorScheme="blue"
+            type="submit"
+            marginTop={3}
+            onClick={handleSubmit}
+          >
             Add +
           </Button>
-        </Form.Item>
-      </Form>
+        </Container>
+      </FormControl>
     </div>
   );
 };
